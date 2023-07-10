fc3505c1ef2e12fb04501dbb052a81add766e813
iordache <faraonu_ginitoru@yahoo.com>
2023-07-11 01:56:49 +0300

    added logging for sql statements and added name for Phone class

--- auth-server/src/main/java/com/gin/models/Phone.java
+++ auth-server/src/main/java/com/gin/models/Phone.java
@@ -1,10 +1,14 @@
 package com.gin.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 
 @Embeddable
 public class Phone {
 
+    @Column(name = "phone_Number1", length = 20)
     private String phoneNumber1;
+
+    @Column(name = "phone_Number2", length = 20)
     private String phoneNumber2;
 }
