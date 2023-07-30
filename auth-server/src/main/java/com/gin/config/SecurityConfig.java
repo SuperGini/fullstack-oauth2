88b3d6d06ba2637aca5423d77b6b973fefcdace7
iordache <faraonu_ginitoru@yahoo.com>
2023-07-30 23:40:51 +0300

    deleted brackets

--- auth-server/src/main/java/com/gin/config/SecurityConfig.java
+++ auth-server/src/main/java/com/gin/config/SecurityConfig.java
@@ -54,14 +54,14 @@ public class SecurityConfig {
         http
                 // Redirect to the login page when not authenticated from the
                 // authorization endpoint
-                .exceptionHandling((exceptions) -> exceptions
+                .exceptionHandling(exceptions -> exceptions
                         .defaultAuthenticationEntryPointFor(
                                 new LoginUrlAuthenticationEntryPoint("/login"),
                                 new MediaTypeRequestMatcher(MediaType.TEXT_HTML)
                         )
                 )
                 // Accept access tokens for User Info and/or Client Registration
-                .oauth2ResourceServer((resourceServer) -> resourceServer
+                .oauth2ResourceServer(resourceServer -> resourceServer
                         .jwt(Customizer.withDefaults()));
 
         return http.build();
